d5ad4dc0e03456f069e50b1ef863f5162ee8b961
suspring <sky_tk@126.com>
2019-04-13 15:29:12 +0800

    kaptcha

--- src/main/java/com/sucl/sbjms/security/SecurityController.java
+++ src/main/java/com/sucl/sbjms/security/SecurityController.java
@@ -1,16 +1,25 @@
 package com.sucl.sbjms.security;
 
+import com.google.code.kaptcha.Constants;
+import com.google.code.kaptcha.Producer;
 import com.sucl.sbjms.core.rem.Message;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authc.pam.UnsupportedTokenException;
+import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 /**
  * @author sucl
@@ -18,6 +27,11 @@ import javax.servlet.http.HttpServletRequest;
  */
 @RestController
 public class SecurityController {
+
+    private static final String KAPTCHA_SESSION_KEY = "kaptcha_session_key";
+
+    @Autowired
+    private Producer producer;
 
     @PostMapping("/dologin")
     public Message login(HttpServletRequest request){
@@ -72,5 +86,22 @@ public class SecurityController {
             msg = "未知的令牌！";
         }
         return msg;
+    }
+
+    @RequestMapping("captcha.jpg")
+    public void captcha(HttpServletResponse response)throws IOException {
+        response.setHeader("Cache-Control", "no-store, no-cache");
+        response.setContentType("image/jpeg");
+
+        //生成文字验证码
+        String text = producer.createText();
+        //生成图片验证码
+        BufferedImage image = producer.createImage(text);
+        //保存到shiro session
+        Session session = SecurityUtils.getSubject().getSession();
+        session.setAttribute(KAPTCHA_SESSION_KEY, text);
+
+        ServletOutputStream out = response.getOutputStream();
+        ImageIO.write(image, "jpg", out);
     }
 }
--- src/main/java/com/sucl/sbjms/security/filter/DefaultFormAuthenticationFilter.java
+++ src/main/java/com/sucl/sbjms/security/filter/DefaultFormAuthenticationFilter.java
@@ -12,11 +12,13 @@ import javax.servlet.ServletResponse;
 /**
  * 登录拦截器
  * 如果交给spring管理则会出现问题
- *
+ * 如果将filter交由spring管理，那么会对所有请求过滤（而在过滤的过程中必然会依赖于shiro的一些对象，比如SecurityManager，但是这个对象很可能还没有构建出来）
+ * 然后出现异常了：UnavailableSecurityManagerException: No SecurityManager accessible to the calling code, either bound to the org.apache.shiro.util.ThreadContext or as a vm static singleton.  This is an invalid application configuration.
+ * 但是shiro的过滤器是针对我们的配置进行指向性过滤
  * @author sucl
  * @date 2019/4/12
  */
-@Component("formAuthc1")
+//@Component("formAuthc")
 public class DefaultFormAuthenticationFilter extends FormAuthenticationFilter {
 
     @Override
